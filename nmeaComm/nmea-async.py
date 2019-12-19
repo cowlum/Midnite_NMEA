4eac36da4807e86f1d2e72d906c18a65cdeec902
cowlum <siestabandit@gmail.com>
2019-12-18 20:30:17 -0800

    Create nmea-async.py
    Proof of concept for Asyncronous program.

---
+++ nmeaComm/nmea-async.py
@@ -0,0 +1,26 @@
+import asyncore
+datalist = []
+
+class EchoHandler(asyncore.dispatcher_with_send):
+
+    def handle_read(self):
+        data = self.recv(8192)
+        if data:
+            datalist.append(data)
+            print(datalist)
+
+class EchoServer(asyncore.dispatcher):
+
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket()
+        self.set_reuse_addr()
+        self.bind((host, port))
+        self.listen(5)
+
+    def handle_accepted(self, sock, addr):
+        print('Incoming connection from %s' % repr(addr))
+        handler = EchoHandler(sock)
+
+server = EchoServer('192.168.0.140', 2010)
+asyncore.loop()
