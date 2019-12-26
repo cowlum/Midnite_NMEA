ecbe7e7ada0ca88b72104176ab02447f044db7c2
cowlum <siestabandit@gmail.com>
2019-12-25 22:06:02 -0800

    Update nmea-hp33a.py

--- nmeaHP33A/nmea-hp33a.py
+++ nmeaHP33A/nmea-hp33a.py
@@ -1,33 +1,40 @@
-from evdev import InputDevice, categorize, ecodes
+import asyncio
+from asyncserial import Serial
 
-device = InputDevice("/dev/ais") # dAISy for tresting - Make HP33A
-for event in device.read_loop():
-    if event.type == ecodes.EV_KEY:
-        print(categorize(event))
 
- -----------------------------------------------------------------------------------       
->> import asyncio
->>> from evdev import InputDevice, categorize, ecodes
+message = "testing callum"
 
->>> dev = InputDevice('/dev/ais')
+async def tcp_client(message, loop):
+	
+	while True:
+		reader, writer = await asyncio.open_connection('192.168.0.140', 2010, loop=loop)
 
->>> async def helper(dev):
-...     async for ev in dev.async_read_loop():
-...         print(repr(ev))
+		print('Send: %r' % message)
+		writer.write(message.encode())
+		await asyncio.sleep(100)
+		#data = await reader.read(100)
+		#print('Received: %r' % data.decode())
 
-import asyncio, evdev
------------------------------------------------------------------------------------
-import asyncio, evdev
+		#print('Close the socket')
+		#writer.close()
 
-mouse = evdev.InputDevice('/dev/input/event4')
-keybd = evdev.InputDevice('/dev/input/event5')
 
-async def print_events(device):
-    async for event in device.async_read_loop():
-        print(device.path, evdev.categorize(event), sep=': ')
-
-for device in mouse, keybd:
-    asyncio.ensure_future(print_events(device))
+async def serial_in():
+	await serial.read() # Drop anything that was already received
+	while True:
+		#print("mark one")
+		line = await serial.readline() # Read a line
+		#print("mark two")
+		print(line)
+		await asyncio.sleep(0) # Let's be a bit greedy, should be adjust to your needs
 
 loop = asyncio.get_event_loop()
+serial = Serial(loop, "/dev/ais", baudrate=34800)
+
+#loop.create_task(connect('first hello', 15))
+#asyncio.ensure_future(test())
+loop.create_task((tcp_client(message, loop)))
+loop.create_task(serial_in())
+
 loop.run_forever()
+loop.close()
