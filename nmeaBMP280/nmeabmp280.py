830bb621839b5de8370016ff16296d0c4895a3f3
Callum <siestabandit@gmail.com>
2019-12-30 20:07:50 -0800

    config parser added to all three scripts

--- nmeaBMP280/nmeabmp280.py
+++ nmeaBMP280/nmeabmp280.py
@@ -1,55 +1,70 @@
-## Python Script to read temp and baromertic pressure from from bmp180
-## Then convert it to nmea and send to the NMEA communicator 
-## Variables to be pulled from /boot/nmea-bmp180.config
-
-import socket
-import sys
+import asyncio
 import pynmea2 
+import smbus2
+import bme280
 import configparser
-import board
-import digitalio
-import busio
-import time
-from adafruit_bmp280 import adafruit_bmp280
 
 
-##--------------------------- Variables -------------------
-config = ConfigParser.ConfigParser()
-config.read("/boot/bmpnmea.config")
-bmp280_enalbed = config.get("BMP280", "bmp280_enalbed")
+config = configparser.ConfigParser()
+config.read("/boot/nmeabmp280.config")
+bmp280_enabled = config.get("BMP280", "bmp280_enabled")
 HOST = config.get("BMP280", "dest_IP")
 PORT = config.get("BMP280", "dest_port")
-#bmp_ic2 = config.get("BMP280", "bmp_ic2")
-#bmp = BMP085(bmp_ic2)  ## This will require ic2tools to check
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+seconds = int(config.get("BMP280", "seconds"))
+
+## Variables
+
+print(HOST)
+print(PORT)
+
+endline = "\n"
+port = 1
+address = 0x76
+bus = smbus2.SMBus(port)
 
 
-## -------check to see if this script should run
-if bmp280_enabled == 'yes':
-	break
-	else:
-	sys.exit()
+calibration_params = bme280.load_calibration_params(bus, address)
+data = bme280.sample(bus, address, calibration_params)
 
-## --- Functions to receive data ---
-def get_pressure():
-    global pressure
-    pressure = str(round(sensor.Pressure(),2))
-    #pressure = str(pressure)
 
-def get_temp():
-    global airtemp
-    airtemp = str(round(sensor.Temperature(),2))
-    #airtemp = str(airtemp)
 
-while True:
-	time.sleep(60)
-	ntemp = pynmea2(nmea_xdr("II", "XDR", "C", get_temp(), "C", "TempSensor"))
-	npres = pynmea2(nmea_xdr("II", "XDR", "P", get_pressure(), "B", "Barometer"))
 
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conn:
-	    conn.connect((HOST, PORT))
-	    #s.sendall(b'Hello, world')
-	    conn.send(str.encode(ntemp))
-	    conn.send(str.encode(npres))
-   
+
+async def tcp_client(message):
+    #reader, writer = await asyncio.open_connection(
+    #    '127.0.0.1', 2000)
+    reader2, writer2 = await asyncio.open_connection(
+        HOST, PORT)
+
+    #print(f'Send: {message!r}')
+    #writer.write(message.encode())
+    #await writer.drain()
+
+    #print(f'Send: {message!r}')
+    #writer2.write(message.encode())
+    #await writer2.drain()
+
+    while True:
+
+        pressure = str(round(data.pressure,1))
+        airtemp = str(round(data.temperature,1))
+
+        ntemp = str(pynmea2.XDR('II','XDR',(('C',airtemp,'C','Temp Sensor',))))
+        ntemp = ntemp + endline
+        ntemp = ntemp.encode() 
+ 
+        npres = str(pynmea2.XDR('II','XDR',(('P',pressure,'B','Pressure Sensor',))))
+        npres = npres + endline
+        npres = npres.encode()        
+
+        writer2.write(ntemp)
+        writer2.write(npres)
+        await writer2.drain()
+        await asyncio.sleep(seconds)
+
+
+    print('Close the connection')
+    writer.close()
+    await writer.wait_closed()
+
+asyncio.run(tcp_client('connecting'))
