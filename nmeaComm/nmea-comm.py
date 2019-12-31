07090746a48945a8acf580b7a1908b99ccbc5659
cowlum <siestabandit@gmail.com>
2019-12-31 11:11:58 -0800

    Update nmea-comm.py
    Removed all coding used during troubleshooting

--- nmeaComm/nmea-comm.py
+++ nmeaComm/nmea-comm.py
@@ -2,21 +2,16 @@ import asyncio
 import configparser
 
 ## Variables
-
 config = configparser.ConfigParser()
 config.read("/boot/nmeacomm.config")
 ncomm_enabled = config.get("nmeaCOMM", "ncomm_enalbed")
 HOST = config.get("nmeaCOMM", "IP")
 PORT = config.get("nmeaCOMM", "port")
 
-print(ncomm_enabled)
-print(HOST)
-
 ## List for the attached devices.
 writers = []
 
 ## Check for enabled
-
 if ncomm_enabled == 'no':
     exit()
 
@@ -29,7 +24,7 @@ def forward(writer, addr, message):
                 #w.write(f"{addr!r}: {message!r}\n".encode())
                 w.write(message.encode())
             except:
-                print("Failure to send")
+                #print("Failure to send")
                 break
 
 ## Function to append new devices to writers list
@@ -39,33 +34,27 @@ def forward(writer, addr, message):
 async def handle(reader, writer):
     writers.append(writer)
     addr = writer.get_extra_info('peername')
-    message = f"{addr!r} is connected !!!!"
-    print(message)
-    #forward(writer, addr, message)
     while True:
         data = await reader.readline()
-        print(data)
         message = data.decode().strip()
         forward(writer, addr, message)
         await writer.drain()
         
         if data is b'':
             try:
-                #print("removing writer")
                 print(addr)
                 writers.remove(writer)
                 writer.close()
                 break
                 #writer.close()
             except:
-                print("data b exceptoin")
-
+                break
 
 async def main():
     server = await asyncio.start_server(
         handle, HOST, PORT)
     addr = server.sockets[0].getsockname()
-    print(f'Serving on {addr}')
+    #print(f'Serving on {addr}')
     async with server:
         await server.serve_forever()
 
