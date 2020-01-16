4103c2423cd421e079f0b9ff02085e6d3a10b897
Callum <siestabandit@gmail.com>
2020-01-15 18:27:44 -0800

    workng version of nmea-RS232 and config added

--- nmeaRS232/nmea-RS232.py
+++ nmeaRS232/nmea-RS232.py
@@ -1,67 +1,59 @@
-## Very likely not working
-## Just a template for what may run
-
+import aioserial
 import asyncio
 import configparser
-import time
 
-## Allow 10 seconds for NmeaCommunicator, i2c to be active
-#time.sleep(10)
+config = configparser.ConfigParser()
+config.read("/home/pi/nmeaRS232.config")
+RS232_enabled = config.get("RS232", "RS232_enabled")
+HOST = config.get("RS232", "dest_IP")
+PORT = config.get("RS232", "dest_port")
+SERIAL = config.get("RS232", "serial_device")
+BAUD = config.get("RS232", "dest_baud")
 
-## Variables
-#config = configparser.ConfigParser()
-#config.read("/home/midnite/nmeaRS232.config")
-#RS232_enabled = config.get("RS232", "bmp280_enabled")
-#HOST = config.get("RS232", "dest_IP")
-#PORT = config.get("RS232", "dest_port")
-#seconds = int(config.get("RS232", "seconds"))
-#endline = "\n"
-
-
-## Check for enabled
 if RS232_enabled == 'no':
     exit()
 
-## Open a connection to Nmea Communicator
-## While True collect pressure and tempreture
-## Send the nmea string compiled by pynmea2 and wait seconds.
-
-## Connect to port
-
-async def handle_echo(reader, writer):
-    asyncio.start_server(handle_echo, HOST, PORT, loop=loop)
+async def handle_echo():
+    print("TCP start")
+    global writer
+    reader, writer = await asyncio.open_connection(
+        HOST, PORT)
     while True:
-            data = await reader.readline()
-            message = data.decode()
-            addr = writer.get_extra_info('peername')
-            print("Received %r from %r" % (message, addr))
+          data = await reader.read(10000)
+           #message = data.decode().strip()
+          # print(data)
+          await send_serial(data)
+          await asyncio.sleep(0)
 
-            print("Send: %r" % message)
-            ## Send to Serial
-            connection_made(self, transport):
-      
 
-class Output(asyncio.Protocol):
-    def connection_made(self, transport):
-        self.transport = transport
-        print('port opened', transport)
-        transport.serial.rts = False
-        #transport.write(b'hello world\n')
+#async def read_and_print(aioserial_instance: aioserial.AioSerial):
+async def read_and_print():
+    global aioserial_instance
+    aioserial_instance = aioserial.AioSerial(port=SERIAL,baudrate=BAUD)
+    print("Serial start")
+    while True:
+        serial = await aioserial_instance.readline_async()
+        print(serial)
+        await send_tcp(serial)
+        await asyncio.sleep(0)
 
-    def data_received(self, data):
-        print('data received', repr(data))
-        ## Send to TCP
-        writer.write(data)
-        self.transport.close()
+async def send_tcp(serial):
+  try:
+    writer.write(serial)
+  except:
+    print("no tcp writer")
+    pass
 
-    def connection_lost(self, exc):
-        print('port closed')
-        asyncio.get_event_loop().stop()
+async def send_serial(data):
+  try:
+     await aioserial_instance.writelines_async(data)
+     #print(data)
+  except:
+    print("No Serial writer")
+    pass
 
-loop = asyncio.get_event_loop()
-tasks = [
-    serial.aio.create_serial_connection(loop, Output, '/dev/ttyUSB0', baudrate=115200),
-    asyncio.start_server(handle_echo, '127.0.0.1', 8888, loop=loop)]
-loop.run_until_complete(tasks)
-loop.run_forever()
-loop.close()
+async def main():
+    await asyncio.gather(handle_echo(),read_and_print())
+
+
+asyncio.run(main())
